3e07d060c6d5990ccdc62b6717189ed4e94c9261
root <root@DESKTOP-QS0QAE8>
2023-08-31 21:37:08 +0100

    pep8 standard

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,6 +8,8 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(),
                          bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Validate password'''
     encoded_bytes = password.encode()
     return bcrypt.checkpw(encoded_bytes, hashed_password)
