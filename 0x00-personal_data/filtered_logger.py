7aa75a248a612a710ac188db297c293dc32e7d37
root <root@DESKTOP-QS0QAE8>
2023-08-31 16:08:14 +0100

    implemetns the format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''filtered_logger'''
 import re
+import logging
 from typing import List
 
 
@@ -11,3 +12,20 @@ def filter_datum(fields: List[str], redaction: str,
         message: str = re.sub(field + '=.*?' + separator, field +
                               '=' + redaction + separator, message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+        """ Redacting Formatter class
+        """
+
+        REDACTION = "***"
+        FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+        SEPARATOR = ";"
+
+        def __init__(self, fields: List[str]):
+            super(RedactingFormatter, self).__init__(self.FORMAT)
+            self.fields = fields
+
+        def format(self, record: logging.LogRecord) -> str:
+            message = super(RedactingFormatter, self).format(record)
+            redacted = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+            return redacted
