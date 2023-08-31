527db63d968083f936276ff3249a8bbb596bf4ef
root <root@DESKTOP-QS0QAE8>
2023-08-31 16:26:34 +0100

    err fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,19 +13,23 @@ def filter_datum(fields: List[str], redaction: str,
                               '=' + redaction + separator, message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
-        """ Redacting Formatter class
-        """
+    """ Redacting Formatter class
+    """
 
-        REDACTION = "***"
-        FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-        SEPARATOR = ";"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-        def __init__(self, fields: List[str]):
-            super(RedactingFormatter, self).__init__(self.FORMAT)
-            self.fields = fields
+    def __init__(self, fields: List[str]):
+        '''init function for the RedactingFormatter class'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
-        def format(self, record: logging.LogRecord) -> str:
-            message = super(RedactingFormatter, self).format(record)
-            redacted = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
-            return redacted
+    def format(self, record: logging.LogRecord) -> str:
+        '''format method'''
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION, message,
+                                self.SEPARATOR)
+        return redacted
