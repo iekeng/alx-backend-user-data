5bd74a7ad63c07c7d89439ccc86cf0e27feb60f2
root <root@DESKTOP-QS0QAE8>
2023-08-31 12:09:50 +0100

    obfuscator

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 '''filtered_logger'''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    '''obfuscates data from fields'''
-    return re.sub(fr'\b({"|".join(fields)})\b', redaction, message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    for field in fields:
+        '''Obfuscates personal details'''
+        message: str = re.sub(
+            field +
+            '=.*?' +
+            separator,
+            field +
+            '=' +
+            redaction +
+            separator,
+            message)
+    return message
