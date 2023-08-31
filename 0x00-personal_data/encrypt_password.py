b6e39dac054acad29a5128248d3ff6c201e7b764
root <root@DESKTOP-QS0QAE8>
2023-08-31 21:34:03 +0100

    pwd check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,11 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     '''password hasher'''
-    return bcrypt.hashpw(password.encode('utf-8'),
+    return bcrypt.hashpw(password.encode(),
                          bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    encoded_bytes = password.encode()
+    return bcrypt.checkpw(encoded_bytes, hashed_password)
