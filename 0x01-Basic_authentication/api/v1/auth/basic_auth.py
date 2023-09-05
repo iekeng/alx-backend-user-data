f04ae43df22d890de68d54aa59e0edb025fcffc8
root <root@DESKTOP-QS0QAE8>
2023-09-05 14:45:41 +0100

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,12 @@
 """
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
+    """ BasicAuth class
+    """
 
     def __init__(self):
+        """ Initialization function
+        """
         pass
