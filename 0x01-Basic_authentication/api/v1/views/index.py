c24a18b8df6e44806260793e269f218da8cc3c08
root <root@DESKTOP-QS0QAE8>
2023-09-05 14:10:26 +0100

    route modification

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,17 +26,17 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauth() -> Response:
     """ GET /api/v1/unauthorized
     Trigger a 401
     """
-    return abort(401)
+    abort(401)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> Response:
     """GET /api/v1/forbidden
     Trigger a 403
     """
-    return abort(403)
+    abort(403)
