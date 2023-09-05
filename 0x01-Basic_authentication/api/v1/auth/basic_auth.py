e480120dd7be7108248f8d48518892aef5158c15
root <root@DESKTOP-QS0QAE8>
2023-09-05 22:04:44 +0100

    basic auth base64 extractor

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,3 +12,16 @@ class BasicAuth(Auth):
         """ Initialization function
         """
         super().__init__()
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Header extraction
+        """
+        if not authorization_header:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+
+        if authorization_header.startswith('Basic '):
+            return authorization_header.split(' ')[-1]
+        return None
