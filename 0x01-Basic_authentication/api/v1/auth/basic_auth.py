7afd5050c63ab3ac86956bdd6d18ccc5e1b25509
root <root@DESKTOP-QS0QAE8>
2023-09-06 07:03:58 +0100

    err fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,6 +59,8 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Retrieves user credentials from header
+        """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
