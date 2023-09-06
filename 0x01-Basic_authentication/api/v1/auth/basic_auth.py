703593a787084202143336c30fdb585a9ad1b17c
root <root@DESKTOP-QS0QAE8>
2023-09-06 21:29:52 +0100

    allow ":" in pwd

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,7 +60,7 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        user_info = decoded_base64_authorization_header.split(':')
+        user_info = decoded_base64_authorization_header.split(':', 1)
         return user_info[0], user_info[1]
 
     def user_object_from_credentials(
