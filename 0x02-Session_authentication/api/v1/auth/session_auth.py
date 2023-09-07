ca883c7257986fa78469ddd1f1f789272c82a051
root <root@DESKTOP-QS0QAE8>
2023-09-07 10:33:28 +0100

    session ad

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,13 +2,28 @@
 """Session auth module
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
 
+    user_id_by_session_id = {}
+
     def __init__(self):
         """Initialization function
-            """
+        """
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates user session
+        """
+        if not user_id:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
