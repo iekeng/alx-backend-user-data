2ef41c1051ae92dce6281c4aca6757f592a49d1c
root <root@DESKTOP-QS0QAE8>
2023-09-07 10:40:40 +0100

    err fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,7 +19,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """ Creates user session
         """
-        if not user_id:
+        if user_id is None:
             return None
 
         if not isinstance(user_id, str):
@@ -27,3 +27,5 @@ class SessionAuth(Auth):
 
         session_id = uuid4()
         self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
