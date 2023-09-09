9e01d105406616e36a12c73e88458be0b55083d7
root <root@DESKTOP-QS0QAE8>
2023-09-09 17:31:29 +0100

    sess auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,18 +4,13 @@
 from api.v1.auth.auth import Auth
 from uuid import uuid4
 from models.user import User
+from flask import jsonify
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
-
     user_id_by_session_id = {}
-
-    def __init__(self):
-        """Initialization function
-        """
-        super().__init__()
 
     def create_session(self, user_id: str = None) -> str:
         """ Creates user session
@@ -26,38 +21,32 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return None
 
-        session_id = uuid4()
+        id = uuid4()
+        session_id = str(id)
+
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Return user with session_id
+        """ Returns User ID based on session_id
         """
-        if session_id is None:
+        if not session_id or not isinstance(session_id, str):
             return None
 
-        if isinstance(session_id, str):
-            try:
-                return user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
 
-            except NameError:
-                return None
-
-        sessions = self.user_id_by_session_id
-
-        if session_id in sessions:
-            return sessions[session_id]
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Return user with session_id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """ Return current user based on session_id
         """
         session_id = self.session_cookie(request)
-
-        if not session_id:
-            return
         user_id = self.user_id_for_session_id(session_id)
 
-        if not user_id:
-            return
         return User.get(user_id)
