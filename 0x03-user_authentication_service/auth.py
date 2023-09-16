e17be4eb09bfc86e9efc6f3fd74e3eae6e8b6598
root <root@DESKTOP-QS0QAE8>
2023-09-16 13:54:26 +0100

    password reset update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,6 +100,18 @@ class Auth:
         except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
-        # self._db.update_user(user.id, reset_token=reset_token)
         user.reset_token = reset_token
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password
+        """
+        user_reset_token = reset_token
+        try:
+            user = self._db.find_user_by(reset_token=user_reset_token)
+            hashed_password = _hash_password(password)
+        except NoResultFound:
+            raise ValueError
+
+        user.hashed_password = hashed_password
+        user.reset_token = None
