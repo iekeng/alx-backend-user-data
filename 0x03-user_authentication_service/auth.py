5c62b9c763977f1a367600417a1842638689c338
root <root@DESKTOP-QS0QAE8>
2023-09-16 13:22:44 +0100

    err fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,16 +92,13 @@ class Auth:
         user = self._db.find_user_by(id=user_id)
         user.session_id = None
 
-    def get_reset_password_token(email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """generate reset token
         """
-        db = self._db
-        token = _generate_uuid()
-
         try:
             user = db.find_user_by(email=email)
-        except (NoResultFound, InvalidRequestError):
+        except NoResultFound:
             raise ValueError
-
-        user.reset_token = token
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
         return token
