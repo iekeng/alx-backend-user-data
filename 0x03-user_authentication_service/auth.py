1b87b44330dbd3c3e773cc3d13dc6d96d728e8df
root <root@DESKTOP-QS0QAE8>
2023-09-16 13:34:03 +0100

    err fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,5 +100,6 @@ class Auth:
         except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
+        # self._db.update_user(user.id, reset_token=reset_token)
+        user.reset_token = reset_token
         return reset_token
